a05962603f5b64694ad9810d2041f3f559a7c65f
lequockhanh19521680 <19521680@gm.uit.edu.vn>
2022-01-04 21:20:14 +0700

    chinhlai datatype

--- backend/src/feedback/app/controllers/feedbackController.js
+++ backend/src/feedback/app/controllers/feedbackController.js
@@ -9,7 +9,7 @@ class FeedbackController {
 
     async get(req, res, next) {
         try {
-            const query = "SELECT * FROM feedback"
+            const query = "SELECT * FROM feedback ORDER BY id DESC"
             const data = await db.query(query)
             const allUsers = data.rows
             res.status(200).json(allUsers)
@@ -22,15 +22,7 @@ class FeedbackController {
     }
     //ham post trong class FeedbackController
     async post(req,res,next) {
-        //khai bao thoi gian thuc
-
-        let today = new Date();
-
-        let year = today.getFullYear();
-        let month = today.getMonth() + 1;
-        let date = today.getDate() +1;
-
-        let now = `${year}/${month}/${date}`
+       
         let read = false;
 
         //khai bao request.body
@@ -39,8 +31,8 @@ class FeedbackController {
 
 
         // query insert vao table feedback, no nhu sql nen de nho lam
-        db.query('INSERT INTO feedback(userid,tittle,content,createat,isread) VALUES ($1, $2, $3, $4, $5 )', 
-        [userId,tittle,content,now,read], (err,results) => {
+        db.query('INSERT INTO feedback(userid,tittle,content,isread) VALUES ($1, $2, $3, $4 )', 
+        [userId,tittle,content,read], (err,results) => {
             // chia thanh 2 truong hop loi va khong loi. Neu loi xay ra thi success: fail tren json
             if(err)
             {
