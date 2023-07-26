95bc29b0712e288fc86208043d7ae1c7958d4cf0
adnan358 <adnanbilac@gmail.com>
2023-07-26 10:00:14 +0300

    Fix

--- docker/base/build_dev.sh
+++ docker/base/build_dev.sh
@@ -4,9 +4,9 @@ pushd "$( dirname $0 )" > /dev/null
 CURDIR=$( pwd -P )
 
 ## build base images
-docker build -t openloyalty/base-nodejs -f nodejs-dockerfile $CURDIR
-docker build -t openloyalty/base-nginx -f nginx-dockerfile $CURDIR
-docker build -t openloyalty/base-php-fpm -f php-fpm-dockerfile $CURDIR
-docker build -t openloyalty/elasticsearch -f elasticsearch-dockerfile $CURDIR
+docker build -t openloyalty/base-nodejs -f nodejs-dockerfile $CURDIR --no-cache
+docker build -t openloyalty/base-nginx -f nginx-dockerfile $CURDIR --no-cache
+docker build -t openloyalty/base-php-fpm -f php-fpm-dockerfile $CURDIR --no-cache
+docker build -t openloyalty/elasticsearch -f elasticsearch-dockerfile $CURDIR --no-cache
 
 popd > /dev/null
